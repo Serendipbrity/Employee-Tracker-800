1f19d83c7661c1d7ee19cc8be9ec1e21fe4c402b
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-10 18:11:49 -0400

    test files and installs to test

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employees (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     title VARCHAR(30) NOT NULL,
