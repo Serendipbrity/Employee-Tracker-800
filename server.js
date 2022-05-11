58bf1632130c37aba0fa5bc22eb07bb8a35f0450
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-11 17:24:42 -0400

    roles/department added to CRUD

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = mysql.createConnection(
     console.log('Connected to the emp_tracker database.')
 );
 
-
+// -------- GET ALL SECTION ------->
 
 // Get all employees
 app.get('/api/employees', (req, res) => {
@@ -39,7 +39,46 @@ app.get('/api/employees', (req, res) => {
         data: rows
       });
     });
+});
+  
+
+// Get all roles
+app.get('/api/roles', (req, res) => {
+    const sql = `SELECT * FROM roles`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
   });
+
+
+
+// Get all departments
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT * FROM department`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+
+// ---------- GET SINGLE SECTION ------>
+  
 
 // Get a single employee
 app.get('/api/employee/:id', (req, res) => {
@@ -57,6 +96,45 @@ app.get('/api/employee/:id', (req, res) => {
       });
     });
   });
+
+
+  // Get a single role
+app.get('/api/role/:id', (req, res) => {
+    const sql = `SELECT * FROM roles WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+
+// Get a single department
+app.get('/api/department/:id', (req, res) => {
+    const sql = `SELECT * FROM department WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+  
+
+//  --------- DELETE SECTION -------->
 
 // Delete an employee
 app.delete('/api/employee/:id', (req, res) => {
@@ -79,6 +157,56 @@ app.delete('/api/employee/:id', (req, res) => {
       }
     });
   });
+
+
+  // Delete a role
+app.delete('/api/role/:id', (req, res) => {
+    const sql = `DELETE FROM roles WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Role not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+
+  // Delete a department
+app.delete('/api/department/:id', (req, res) => {
+    const sql = `DELETE FROM department WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Department not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+
+// ------ CREATE SECTION ----------> 
+
 // create an employee
 app.post('/api/employee', ({ body }, res) => {
     const errors = inputCheck(body,
@@ -107,6 +235,57 @@ app.post('/api/employee', ({ body }, res) => {
         });
     });
 });
+
+// create a role
+app.post('/api/roles', ({ body }, res) => {
+    const errors = inputCheck(body,
+        'title',
+        'department',
+        'salary'
+    );
+    if (errors) {
+        res.status(400).json({ error: errors });
+    }
+    const sql = `INSERT INTO roles (title, department, salary)
+    VALUES (?,?,?,?,?,?)`
+    const params = [body.title, body.department, body.salary];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// create a department
+app.post('/api/department', ({ body }, res) => {
+    const errors = inputCheck(body,
+       'names'
+    );
+    if (errors) {
+        res.status(400).json({ error: errors });
+    }
+    const sql = `INSERT INTO roles (names)
+    VALUES (?,?,?,?,?,?)`
+    const params = [body.names];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
