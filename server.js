dc85a3bde9599619b4318d1e94444cbb2e781cb3
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-11 14:58:51 -0400

    get/create/delete employee

--- server.js
+++ server.js
@@ -23,9 +23,37 @@ const db = mysql.createConnection(
 );
 
 // db object using query method and executes a callback with all resulting rows matching query
-db.query('Select * FROM employees', (err, rows) => {
-    console.log(rows);
-});
+// db.query('Select * FROM employees', (err, rows) => {
+//     console.log(rows);
+// });
+
+// get single employee
+// db.query(`Select * FROM employees WHERE id= 8`, (err, rows) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(rows);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM employees WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(result);
+// });
+
+// create a candidate
+// const sql = `INSERT INTO employees (id, first_name, last_name, title, department, salary, manager)
+//                 VALUES(?,?,?,?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 'Sales Lead', 'Sales', 100000, null];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
